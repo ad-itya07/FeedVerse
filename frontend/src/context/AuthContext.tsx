ce652611543930537f63e994de46f38fba2ab816
ad_itya07 <jhaaditya2207@gmail.com>
2024-11-29 19:44:55 +0530

    conext api for Authn

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,63 @@
+import React, { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { supabase } from "../supabase/SupabaseClient";
+import { UserContextType } from "../types/contextTypes";
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const useAuth = (): UserContextType => {
+  const user = useContext(UserContext);
+  if (!user) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return user;
+};
+
+interface AuthProviderProps {
+    children: ReactNode;
+  }
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const userSession = async () => {
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.getSession();
+
+      if (error) {
+        console.error("Error fetching data: ", error);
+      } else {
+        setCurrentUser(session?.user);
+      }
+      setIsLoading(false);
+    };
+
+    userSession();
+
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        setCurrentUser(session?.user || null);
+        setIsLoading(false);
+      }
+    );
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+  }, []);
+
+  const logout = async () => {
+    await supabase.auth.signOut();
+    setCurrentUser(null);
+  };
+
+  const value = {
+    currentUser,
+    isLoading,
+    logout,
+  };
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
