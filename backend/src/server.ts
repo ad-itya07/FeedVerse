0b0d90fe8457848d0e060a06cddca3846ce426de
ad_itya07 <jhaaditya2207@gmail.com>
2024-11-30 20:41:44 +0530

    minor env change

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,12 +6,13 @@ import { router as userRoute} from "./routes/userRoute";
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 7000
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/user' , userRoute)
 
-app.listen(3000 , () => {
-    console.log(`App running on port 3000`);
+app.listen(port , () => {
+    console.log(`App running on port ${port}`);
 })
