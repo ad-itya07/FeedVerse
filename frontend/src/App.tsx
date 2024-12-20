2681ecf6821f11d22a73e11017ed20a2b2d03bdd
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-20 22:53:18 +0530

    added Landing page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,23 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import HomePage from "./pages/HomePage.tsx";
 import FeedsLayout from "./layouts/FeedsLayout.tsx";
+import LandingPageLayout from "./layouts/LandingPageLayout.tsx";
+import LandingPage from "./pages/LandingPage.tsx";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route
+      <Route
           path="/"
+          element={
+            <LandingPageLayout>
+              <LandingPage />
+            </LandingPageLayout>
+          }
+        />
+        <Route
+          path="/home"
           element={
             <FeedsLayout>
               <HomePage />
